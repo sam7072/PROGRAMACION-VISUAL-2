6f7f595aca24befa97c7c62e4b779a4485bdc5eb
sam7072 <59964609+sam7072@users.noreply.github.com>
2022-01-21 08:08:47 -0300

    model

--- Libreria/modeloDB/DBLibreria.cs
+++ Libreria/modeloDB/DBLibreria.cs
@@ -28,6 +28,111 @@ namespace modelo
             optionsBuilder.UseSqlServer(connSQL);
         
         }
+        //configurar el modelo de objetos
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            //Cardinalidad de las clases 
+
+            model.Entity<DemandaLibreria>()
+                .HasOne(Dem => Dem.presupuesto)
+                .WithMany(Pre => Pre.DemandaLibrerias)
+                .HasForeignKey(Dem => Dem.PresupuestoId);
+
+
+            model.Entity<Distribuidores>()
+                .HasOne(Dis => Dis.presupuesto)
+                .WithMany(Pre => Pre.Distribuidores)
+                .HasForeignKey(Dis => Dis.PresupuestoId);
+
+
+
+
+            //Configuracion 
+            model.Entity<InventarioLibro>()
+                .HasOne(inventario => inventario.presupuesto)
+                .WithMany(lib => lib.inventarioLibros)
+                .HasForeignKey(inventario => inventario.PresupuestoId);
+
+
+
+
+
+            /* cardinalidad 1 a 1
+            model.Entity<Inventario>()
+            .HasOne
+            */
+
+
+
+
+            /*
+
+            // configurar el modelo de clases 
+            model.Entity<Configuracion>()
+                .HasNokey();
+            //periodo 
+            model.Entity<Configuracion>()
+                .HasOne(Confi => Confi.PeriodoVigente)
+                .WithMany()
+                .HasForeignKey(Confi => Confi.PeriodoVigente);
+
+
+
+            //Prerequisitos
+            //Malla
+            model.Entity<Prerequisito>()
+                .HasOne(prerequisito => prerequisito.Prerequisito)
+                .WithMany(malla => malla.Prerequisitos)
+                .HasForeignKey(prerequisito => prerequisito.MallaId);
+
+            //Materia
+            model.Entity<Prerequisito>()
+                .HasOne(prerequisito => prerequisito.Prerequisito)
+                .WithMany(materia => materia.Prerequisitos)
+                .HasForeignKey(prerequisito => prerequisito.MateriaId);
+
+
+            //clave primaria formada por dos claves foreneas
+            model.Entity<Prequisitos>()
+                .HasKey(prerequisito => new
+                {
+                    prerequisito.MallaId,
+                    prerequisito.MateriaId
+                });
+
+            //Matricula Det
+            //-calificacion
+            model.Entity<Matricula_Det>()
+                .HasOne(matricula_det => matricula_det.calificaciones)
+                .WithOne(calificacion => calificacion.Matricula_det)
+                .HasForeignKey<Calificacion>(calificacion => calificacion.Matricula_detID);
+
+
+            //malla-materia
+            // de one a one
+            model.Entity<Malla>()
+                .HasOne(malla => malla.Materia)
+                .WithMany(materia => materia.Malla)
+                .HasForeignKey<Malla>(malla => malla.MateriaId);
+
+
+            //-carrera
+            model.Entity<Malla>()
+                .HasOne(malla => malla.Carrera)
+                .WithMany()
+                .HasForeignKey(malla => malla.CarreraID);
+
+            */
+
+
+
+
 
         }
+
+
+
+
+
+    }
 }
